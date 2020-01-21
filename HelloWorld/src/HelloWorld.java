1a62ca0641df9d51b9c65b37b7c280af6fb1dded
User <User@YD02-03>
2020-01-21 12:37:20 +0900

    test

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+	/*
+	 * 여러 라인을 주석처리 할때 사용합니다. 작성자: 이창호 2020.01.21
+	 */
+	public static void main(String[] args) {
+		// Hello World 출력하는 구문.
+		int age = 26;
+		int year = 2020;
+		System.out.println("Hello, world");
+		System.out.println("자바는 재밌는 언어.");
+		System.out.println("올해는" + year + "년");
+		System.out.println("내 나이는 " + age + "입니다.");
+	}
+
+}
