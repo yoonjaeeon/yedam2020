0cf6e5b344468197f656dc06e684c900f2eacc0f
User <User@YD02-03>
2020-01-22 17:55:42 +0900

    ss

--- HelloWorld/src/ForExample.java
+++ HelloWorld/src/ForExample.java
@@ -1,14 +1,24 @@
 
 public class ForExample {
 	public static void main(String[] args) {
-		 char chr = 65;
-		 for(int i= 0; i< 26; i++) {
-			System.out.println(chr);
-			chr = (char) (chr + 1);
+//		 char chr = 65;
+//		 for(int i= 0; i< 26; i++) {
+//			System.out.println(chr);
+//			chr = (char) (chr + 1);
+//	}
+//		 boolean trueOrFalse = false;
+	//	 if (3 !=5) {
+	//		 System.out.println("참입니다.");
+	//}
+//	int sum = 0;
+//	for (int i = 1; i <= 10; i++)
+	//	if (i % 2 ==0 ) {
+    //    sum = sum + i;
+//		System.out.println("i: " + i + ", sum: " + sum);
+//	}
+	int n = 3;
+	for (int i = 1; i <=9; i++) {
+		System.out.println(n + "x" + i + "=" + (n*i));
 	}
-		 boolean trueOrFalse = false;
-		 if (3 !=5) {
-			 System.out.println("참입니다.");
-		 }
 }
 }
