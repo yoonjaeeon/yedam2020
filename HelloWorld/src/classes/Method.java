40874898e5f2aad0fa3b79ef793f060724d0f9bf
User <User@YD02-03>
2020-01-30 17:40:31 +0900

    0130

---
+++ HelloWorld/src/classes/Method.java
@@ -0,0 +1,51 @@
+package classes;
+
+public class Method {
+//	public static void main(String[] args) {
+//		int i = sum(1, 2); // return값 반환 메소드
+//		System.out.println(i);
+//		int num1 = 10, num2 = 15;
+//		int result = sum(num1,num2) * minus(num1,num2); //num1,num2의 값을 sum 과  miuns의 결과값 return 값이 있는 메소드
+//		System.out.println("결과값은: "+result);
+	int leftField;
+	int rightField;
+
+//	}  //end of main                                            
+//	public static void sum(int a, int b) { // return 값이 없는 메소드
+//		System.out.println(a + b);
+//	}
+	public int getLeftField() {
+		return leftField;
+	}
+
+	public void setLeftField(int leftField) {
+		this.leftField = leftField;
+	}
+
+	public int getRightField() {
+		return rightField;
+	}
+
+	public void setRightField(int rightField) {
+		this.rightField = rightField;
+	}
+
+	public int thisSum() { 
+		return leftField + rightField; //반환
+	}
+
+	public int thisMinus() { 
+		return leftField - rightField; //반환
+	}
+
+	public int sum(int a, int b) {
+		int result = 0;
+		result = a + b; // 결과를 반환
+		return result;
+	}
+
+	public int minus(int a, int b) {
+		return a - b; // 결과를 반환
+	}
+
+}
---
+++ HelloWorld/src/classes/MethodExample.java
@@ -0,0 +1,24 @@
+package classes;
+
+public class MethodExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Method m = new Method(); //인스턴스 생성 m-> leftField,right
+		m.setLeftField(30); //메소드 m.leftField -> 30
+		m.setRightField(4);	//메소드 m.rightField -> 4
+		int result = m.thisSum() + m.thisMinus(); //연산 필드에 있는 값을 받고 매개값이 없다 sum=30+4 minus=30-4
+		int result2 = m.sum(20, 10) * m.minus(30, 25); //연산 필드랑 상관없이 매개값을 받는다
+		//출력
+		System.out.println("1필드의 계산결과: "+result);
+		System.out.println("1두매개값의 계산결과: "+result2);
+		
+		m.setLeftField(100);//메소드 m.leftField -> 100
+		m.setRightField(50);//메소드 m.rightField -> 50
+		result = m.thisSum() + m.thisMinus(); // 필드에 있는 값을 받고 매개값이 없다
+		System.out.println("2필드의 계산결과: "+result);
+		
+		m = null; // 위의 값이 null로 변경
+ 		System.out.println(m.getLeftField());
+	}
+}
